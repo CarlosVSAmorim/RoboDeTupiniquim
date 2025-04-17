6b3501a3393bd8feb9f1e87f370d158415c65b54
Carlos Vinicius <kakavinicius83@gmail.com>
2025-04-17 18:59:29 -0300

    Comecando o codigo, parte de entrada do usuario.

---
+++ RoboDeTupiniquim/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Bem-vindo ao sistema de controle do Robô Tupiniquim.");
+Console.WriteLine("Digite o tamanho do grid retangular. Exemplo de entrada: 5 5");
+string[] dimensoes = Console.ReadLine().Split(' ');
+int maxX = int.Parse(dimensoes[0]);
+int maxY = int.Parse(dimensoes[1]);
+
+int numeroRobo = 1;
